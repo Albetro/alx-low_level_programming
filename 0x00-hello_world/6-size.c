32c6116465d62d8217a41e7220b9f41e4e7e8b51
Albetro <albertxang@gmail.com>
2023-02-16 15:10:17 -0800

    beginner program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - program that prints the sizes of various types on the computer
- *
- * Return Always 0 (Success)
- */
+*main - Entry point
+*
+*Return:Always 0 (success)
+*/
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
-
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
-
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
-
 	return (0);
 }
-
