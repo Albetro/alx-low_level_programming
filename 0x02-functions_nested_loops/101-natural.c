1c3c0d6e093f33706fe1a762adbc9697310a298b
Albetro <albertxang@gmail.com>
2023-02-21 17:08:25 -0800

    Nature made the natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints natural numbers below 1024 that are
+ * multiples of 3 or 5
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, z;
+
+	for (i = 1; a < 1024; i++)
+{
+	if ((i % 3) == 0 || (i % 5) == 0)
+	z += i;
+}
+	printf("%d\n", z);
+	return (0);
+}
