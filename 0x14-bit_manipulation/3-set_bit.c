a98d7d0342965f63be30fced22aa52ff6c248f59
africiak <albertxang@gmail.com>
2023-04-11 01:37:10 +0300

    bitwise 4

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * Set_bit - a code that sets the value of a bit at a given index to 1.
- *
+ * set_bit - Sets the value of a bit at a given index to 1.
  * @n: A pointer to the bit.
- * 
  * @index: The index to set the value at - indices start at 0.
  *
  * Return: If an error occurs - -1.
