e675579824627d8cda0fa2d32fae2c08b826f718
africiak <albertxang@gmail.com>
2023-04-11 02:53:00 +0300

    2 bitwise 5

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /*
  * File: 100-elf_header.c
  */
 /**
  * check_elf - Checks if a file is an ELF file.
- * 
  * @e_ident: A pointer to an array containing the ELF magic numbers.
  *
  * Description: If the file is not an ELF file - exit code 98.
