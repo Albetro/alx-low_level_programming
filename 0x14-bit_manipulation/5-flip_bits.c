1778354f6943b7082c2e7c045c5795a70e07448d
africiak <albertxang@gmail.com>
2023-04-11 01:51:15 +0300

    bitwise 6

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,10 +4,8 @@
  * flip_bits - Counts the number of bits needed to be flipped to get from one number to another.
  * 
  * @n: The number.
- * 
- * @m: The number to flip n to.
- *
- * Return: The necessary number of bits to flip to get from n to m.
+ *  @m: The number to flip n to.
+ *Return: The necessary number of bits to flip to get from n to m.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
