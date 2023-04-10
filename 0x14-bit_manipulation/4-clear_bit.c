22156bccc65cbfe2dec274b60ed2001b702fab9f
africiak <albertxang@gmail.com>
2023-04-11 01:39:32 +0300

    bitwise 5

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * a code that sets the value of a bit at a given index to 0.
- *
+ * clear_bit - Sets the value of a bit at a given index to 0.
  * @n: A pointer to the bit.
- * 
  * @index: The index to set the value at - indices start at 0.
  *
  * Return: If an error occurs - -1.
