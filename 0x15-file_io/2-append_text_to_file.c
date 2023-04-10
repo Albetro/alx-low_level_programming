a248ec08dd66b51cb4a6335fc258e1c95cee4f36
africiak <albertxang@gmail.com>
2023-04-11 02:46:20 +0300

    2 bitwise 3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,13 +2,11 @@
 #include "main.h"
 
 /**
- * a code that  appends text at the end of a file
- *
+ * append_text_to_file - appends text at the end of a file
  * @filename: filename.
- * 
  * @text_content: added content.
  *
- * Return: 1 if the file exists. -1 if the file does not exist
+ * Return: 1 if the file exists. -1 if the file  does not exist
  * or if it fails.
  */
 int append_text_to_file(const char *filename, char *text_content)
