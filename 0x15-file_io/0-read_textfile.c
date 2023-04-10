c0f8b2293729682ea4136fc60aca1c13e0d7f027
africiak <albertxang@gmail.com>
2023-04-11 02:24:54 +0300

    2 bitwise 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
- *a code that reads a text file and prints the letters
- *
+ * read_textfile - reads a text file and prints the letters
  * @filename: filename.
- * 
  * @letters: numbers of letters printed.
  *
- * Return: numbers of letters printed. If fail, returns 0.
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
