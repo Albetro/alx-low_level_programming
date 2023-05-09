8a06d151c08236a214d2543a3e14afd1d7477465
Albetro <albertxang@gmail.com>
2023-05-09 05:28:59 +0300

    file input output

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,9 +3,9 @@
 /**
  * create_file - creates a file
  * @filename: filename.
- * @text_content: content writen in the file.
+ * @text_content: content writed in the file.
  *
- * Return: 1 if succcess. -1 if fail
+ * Return: 1 if it success. -1 if  fail
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -15,10 +15,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
-		return (-1)
+		return (-1);
 
 	if (!text_content)
 		text_content = "";
