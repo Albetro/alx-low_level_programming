1fca23f85e9369764e42e558495934689de75ff3
Albetro <albertxang@gmail.com>
2023-06-15 18:39:16 +0300

    List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,13 +7,14 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h)
-    {
-        count++;
-        h = h->next;
-    }
+	while (h)
+	{
+	count++;
+	h = h->next;
+	}
 
-    return count;
+	return (count);
 }
+
