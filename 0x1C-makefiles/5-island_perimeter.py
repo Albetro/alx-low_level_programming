e9f5020f11f8dd17d66519daf8bb421b7e9b2a97
Albetro <albertxang@gmail.com>
2023-08-31 17:51:41 +0300

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-"""Defines the island  perimeter measuring function."""
+"""Defines an island perimeter measuring function."""
 
 def island_perimeter(grid):
-    """return the perimeter of the island
+    """Return the perimiter of an island.
 
     The grid represents water by 0 and land by 1.
 
     Args:
-        grid (lists): A list of integers representing the island.
+        grid (list): A list of list of integers representing an island.
     Returns:
-        The perimeter of the island described in grid."""
+        The perimeter of the island defined in grid.
+    """
     width = len(grid[0])
     height = len(grid)
-    egdes = 0
+    edges = 0
     size = 0
-    
+
     for i in range(height):
-        for l in range(width):
-            if grid[i][l] ==1:
-                size += 1:
-                if (l > 0 and grid[i][l - 1] ==1):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
                     edges += 1
-                if (i > 0 and grid[i - 1][l] ==1):
+                if (i > 0 and grid[i - 1][j] == 1):
                     edges += 1
     return size * 4 - edges * 2
